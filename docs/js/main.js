5cbb3a83a924f10b7778f43fe10da820d6685a3b
Lucas <lcas83539@gmail.com>
2021-04-26 13:08:45 -0300

    mi primer commit de esta pagina

---
+++ docs/js/main.js
@@ -0,0 +1,34 @@
+window.sr = ScrollReveal();
+tiempoDeEspera = 500;
+tiempoDeDistancia = 1000;
+sr.reveal('header', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-70px'
+});
+sr.reveal('.cont__block', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-200px'
+});
+sr.reveal('.image', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-200px'
+});
+sr.reveal('.block__titulo', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-200px'
+});
+sr.reveal('.block__parrafo', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-200px'
+});
+sr.reveal('.block__parrafo > ul > li', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-200px'
+});
+
